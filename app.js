b166e2909f5aeae14af670119f006a7376dc0d4c
ANA JÚLIA SILVA MACEDO <24122496@CFP127ALUNO.LOCAL>
2025-02-06 12:07:50 -0300

    iniciando cards

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 const diciplinas = [
-    {nome: 'PPDM', incon: 'img4.png'},
-    {nome: 'PRO', incon: 'img1.png'},
-    {nome: 'PWBE', icon: 'img3.png'},
-    {nome: 'PWFE', icon: 'img2.png'}
+    {nome: 'PPDM', incon: 'img4.png', cor: 'blue'},
+    {nome: 'PRO', incon: 'img1.png', cor: 'tomato'},
+    {nome: 'PWBE', icon: 'img3.png', cor: 'pink'},
+    {nome: 'PWFE', icon: 'img2.png', cor:'plum'}
 ]
 function criarMenu(diciplina){
     const novoItem = document.createElement('li') //criando um elemento
@@ -16,9 +16,33 @@ function criarMenu(diciplina){
     //colocando as propriedades do img
     novaImagem.src = `./img/${diciplina.icon}`
     novoSpan.textContent = diciplina.nome //textcontent para colocar algo dentro da tag
+    novoItem.style = `--cor-hover: ${diciplina.cor}`
+
+
     novoItem.appendChild(novaImagem) //adiciona um filho
     novoItem.appendChild(novoSpan)
     lista.appendChild(novoItem)
+    
 }
 
 diciplinas.forEach(criarMenu)
+
+//criação dos card
+
+const produtos =[
+    {nome: 'Curso PPDM', imagem:'mobileimg.jfif', cor:'#FCFCFC', duracao: '3M'},
+    {nome: 'Curso PRO', imagem: 'projetosimg.jfif', cor:'#FCFCFC', duracao: '5M'},
+    {nome: 'Curso PWBE', imagem:'backimg.jfif', cor:'#FCFCFC', duracao: '8M'},
+    {nome: 'Curso PWFE', imagem:'frontimg.jfif', cor:'#FCFCFC', duracao: '6M'}
+]
+
+function criarCard (produto){
+    const teste = document.createElement('span')
+    const card = document.getElementById('cardProduto')
+
+    card.appendChild(teste)
+
+    teste.textContent = produto.nome
+}
+
+produtos.forEach(criarCard)
