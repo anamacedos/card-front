7412145b0a2b4932ff72efb24dc7da7172b1543a
ANA JÚLIA SILVA MACEDO <24122496@CFP127ALUNO.LOCAL>
2025-02-06 12:27:01 -0300

    dia 1 finalizado

--- app.js
+++ app.js
@@ -30,19 +30,33 @@ diciplinas.forEach(criarMenu)
 //criação dos card
 
 const produtos =[
-    {nome: 'Curso PPDM', imagem:'mobileimg.jfif', cor:'#FCFCFC', duracao: '3M'},
+    {nome: 'Curso PPDM', descricao: 'Crie aplicativos móveis nativos e híbridos para iOS e Android.', imagem:'mobileimg.jfif', cor:'#FCFCFC', duracao: '3M'},
     {nome: 'Curso PRO', imagem: 'projetosimg.jfif', cor:'#FCFCFC', duracao: '5M'},
-    {nome: 'Curso PWBE', imagem:'backimg.jfif', cor:'#FCFCFC', duracao: '8M'},
-    {nome: 'Curso PWFE', imagem:'frontimg.jfif', cor:'#FCFCFC', duracao: '6M'}
+    {nome: 'Curso PWBE', descricao: 'Aprenda a desenvolver a lógica e os servidores de aplicativos, trabalhando com APIs e bancos de dados', imagem:'backimg.jfif', cor:'#FCFCFC', duracao: '8M'},
+    {nome: 'Curso PWFE', descricao:'Aprenda a gerenciar projetos de software com metodologias ágeis, com entregas de qualidade.', descricao:'Desenvolva interfaces interativas e responsivas usando HTML, CSS, JavaScript e frameworks modernos.', imagem:'frontimg.jfif', cor:'#FCFCFC', duracao: '6M'}
 ]
 
 function criarCard (produto){
-    const teste = document.createElement('span')
-    const card = document.getElementById('cardProduto')
+    //criando todos os elementos do card
+    const novoCard = document.createElement('div')
+    const novaImagem = document.createElement('img')
+    const novoTitulo = document.createElement('h2')
+    const novaDescricao = document.createElement('p')
+    const novoContainerDuracao = document.createElement('div')
+    const novaDuracao = document.createElement('span')
+    const novoBotao = document.createElement('button')
+    const ContainerGeral = document.getElementById('containerCards') //pegando a div criada no html, para depois colocarmos tudo dentro dela
 
-    card.appendChild(teste)
+    
 
-    teste.textContent = produto.nome
+
+    //teste:
+    // const teste = document.createElement('span')
+    // const card = document.getElementById('containerCards')
+
+    // card.appendChild(teste)
+
+    // teste.textContent = produto.nome
 }
 
 produtos.forEach(criarCard)
